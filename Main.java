68ed6dfd777408cb8ca4bbb5e754af446bd20cab
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-17 11:04:03 +0530

    super key & super() = super call

--- Main.java
+++ Main.java
@@ -1,37 +1,40 @@
 class Main{
-	String name;
-	int id;
-	int salary;
-	int age;
-	String location;
-	String cName;
-	
-	public Main(String name, int id, int salary, int age, String location, String cName){
-		//Assigning value to the instance variable using this keyword
-		this.name = name;
-		this.id= id;
-		this.salary = salary;
-		this.age= age;
-		this.location = location;
-		this.cName = cName;
-	}
-
-	public Main(){
-		//Constructor calling using this()
-		this("Rushikesh", 10, 1000000, 24, "Pune", "Google");
-	}
-
-	public void info(){
-		System.out.println(name);
-		System.out.println(id);
-		System.out.println(salary);
-		System.out.println(age);
-		System.out.println(location);
-		System.out.println(cName);
-	}
-
 	public static void main(String [] args){
-		Main tk = new Main();
-		tk.info();
+		Car c = new Car("Thar", 1000000, "Car", "Mahindra");
+		c.displayCar();
+	}
+}
+
+//Parent Class
+class Vehicle{
+	String category;
+	String brand;
+	
+	public Vehicle(String category, String brand){
+		this.category = category;
+		this.brand = brand;
+	}
+	
+	public void displayVehicle(){
+		System.out.println(category);
+		System.out.println(brand);
+	}
+}
+
+//Child Class
+class Car extends Vehicle{
+	String model;
+	int price;
+
+	public Car(String model, int price, String category, String brand){
+		super(category, brand);
+		this.model = model;
+		this.price = price;
+	}
+
+	public void displayCar(){
+		super.displayVehicle();
+		System.out.println(model);
+		System.out.println(price);
 	}
 }
